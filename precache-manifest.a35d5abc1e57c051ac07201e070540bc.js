c1af430fec681ec4467ceddf97d7fd303d6c0639
johnyun <johnyun930@gmail.com>
2020-01-25 00:43:47 -0800

    Updates

---
+++ precache-manifest.a35d5abc1e57c051ac07201e070540bc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da00e059135c5ded4e3b70be8f3fd43",
+    "url": "/churchsite/index.html"
+  },
+  {
+    "revision": "e97814b7bb83ca3cb3d1",
+    "url": "/churchsite/static/css/main.0feb9306.chunk.css"
+  },
+  {
+    "revision": "cc436d2e0496f9441a0a",
+    "url": "/churchsite/static/js/2.12709d46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/churchsite/static/js/2.12709d46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e97814b7bb83ca3cb3d1",
+    "url": "/churchsite/static/js/main.b1c1030c.chunk.js"
+  },
+  {
+    "revision": "0fd00e4775b073812596",
+    "url": "/churchsite/static/js/runtime-main.f1ede7a3.js"
+  },
+  {
+    "revision": "bd828e83f9edd1580febf1901cbd2c89",
+    "url": "/churchsite/static/media/address.bd828e83.jpg"
+  },
+  {
+    "revision": "9cd53206ee9991f8a25b0b134825be73",
+    "url": "/churchsite/static/media/contact.9cd53206.jpg"
+  },
+  {
+    "revision": "e5f80ab32968d8c00bdac34e933b78eb",
+    "url": "/churchsite/static/media/intro.e5f80ab3.jpg"
+  },
+  {
+    "revision": "52507751e44bcda4a3f6f599508a2516",
+    "url": "/churchsite/static/media/location.52507751.jpg"
+  },
+  {
+    "revision": "429623075037301ba185c5785c45d42b",
+    "url": "/churchsite/static/media/main1.42962307.jpg"
+  },
+  {
+    "revision": "a985d6c95cbecd113e91f26d1dcd64df",
+    "url": "/churchsite/static/media/main2.a985d6c9.jpg"
+  },
+  {
+    "revision": "d4e5a2c71b89b7fdfdd109dd86529288",
+    "url": "/churchsite/static/media/main3.d4e5a2c7.jpg"
+  },
+  {
+    "revision": "000f46dbb98c8abfab017337f5648819",
+    "url": "/churchsite/static/media/message.000f46db.jpg"
+  },
+  {
+    "revision": "09cda1460a9114c62fd489e56bdf77f1",
+    "url": "/churchsite/static/media/sermon.09cda146.jpg"
+  },
+  {
+    "revision": "214bf91d761a07e074fdf30a33e2fe86",
+    "url": "/churchsite/static/media/service2.214bf91d.jpg"
+  },
+  {
+    "revision": "d4e5a2c71b89b7fdfdd109dd86529288",
+    "url": "/churchsite/static/media/story.d4e5a2c7.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/churchsite/precache-manifest.a35d5abc1e57c051ac07201e070540bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/churchsite/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
